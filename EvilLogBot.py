4e4805434df122502c8bea3bed15141da0fe4f89
Kulverstukas <kulverstukas.k@gmail.com>
2015-12-31 13:37:48 +0200

    Main class stub

---
+++ EvilLogBot.py
@@ -0,0 +1,12 @@
+'''
+    Author: Kulverstukas
+    Date: 2015.12.31
+    Website: 9v.lt; Evilzone.org
+    Description:
+        EvilLogBot logs channel messages in raw format to an SQLite database and pulls out a
+        specified amount of days worth of logs to be used. This bot is primarily developed to
+        be used with PISG with a logrotate functionality.
+'''
+
+class ExportLog:
+    pass
