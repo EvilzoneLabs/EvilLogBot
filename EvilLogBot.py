edb3fb99f94aaad86f5a511846bdf6c0327c8934
Kulverstukas <kulverstukas@kaime.lt>
2016-01-01 20:12:27 +0200

    Bot now connects to the server, joins channel, rejoins on kick, skips status messages

--- EvilLogBot.py
+++ EvilLogBot.py
@@ -8,5 +8,59 @@
         be used with PISG with a logrotate functionality.
 '''
 
-class ExportLog:
-    pass
+import socket
+import string
+import time
+
+configs = {
+    "server": "irc.evilzone.org",
+    "channel": "#test",
+    "port": 6667,
+    "name": "StatBot"
+}
+
+def containsStatusId(data):
+    return ((data == "451") or
+        (data == "001") or
+        (data == "002") or
+        (data == "003") or
+        (data == "004") or
+        (data == "005") or
+        (data == "251") or
+        (data == "252") or
+        (data == "254") or
+        (data == "255") or
+        (data == "265") or
+        (data == "266") or
+        (data == "375") or
+        (data == "372") or
+        (data == "376") or
+        (data == "332") or
+        (data == "333") or
+        (data == "353") or
+        (data == "422") or
+        (data == "366"))
+
+def begin():
+    joined = False
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    irc.connect((configs["server"], configs["port"]))
+    irc.send("NICK %s\r\n" % configs["name"])
+    irc.send("USER {0} {0} {0} :Evil{0}\r\n".format(configs["name"]))
+    while True:
+        data = ""
+        data = irc.recv(512)
+        if (data.strip() != ""):
+            dataParts = data.split()
+            if "PING" in dataParts[0]: irc.send("PONG %s\r\n" % data.split(" :")[1])
+            elif (not containsStatusId(dataParts[1]) and (dataParts[0][0] == ":")):
+                print data
+        
+        if (not joined):
+            irc.send("JOIN %s\r\n" % configs["channel"])
+            joined = True
+            
+        if ((dataParts[1] == "KICK") and (dataParts[3] == configs["name"])):
+            irc.send("JOIN %s\r\n" % configs["channel"])
+            
+begin()
